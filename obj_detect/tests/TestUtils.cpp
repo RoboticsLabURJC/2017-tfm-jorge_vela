16a09939c1622df8fe535aab96f4d41959d43ffe
José M. Buenaposada <josemiguel.buenaposada@urjc.es>
2020-09-28 01:24:02 +0200

    Compltado test de toda la imagen de TestUtils.TestResampleColorImage

--- obj_detect/tests/TestUtils.cpp
+++ obj_detect/tests/TestUtils.cpp
@@ -58,7 +58,7 @@ public:
 
 TEST_F(TestUtils, TestResampleColorImage)
 {
-  cv::Mat imageMatlab = cv::imread("images/mask_image.jpg", cv::IMREAD_COLOR); 
+//  cv::Mat imageMatlab = cv::imread("images/mask_image.jpg", cv::IMREAD_COLOR);
   cv::Mat image = cv::imread("images/index.jpeg", cv::IMREAD_COLOR); 
   int h = 0;
   int w = 0;
@@ -80,15 +80,10 @@ TEST_F(TestUtils, TestResampleColorImage)
     w = image.size().width / 2 + 1;
   }
 
-  cv::Mat dst = ImgResample(image, w, h);
-  transpose(dst, dst); // JM: Si esto es necesario es porque está guardado mal en el YAML.
-
-  cv::Mat bgr_dst[3];
-  split(dst, bgr_dst);
-
-  cv::Mat bgr_resample[3];
-  split(imageMatlab, bgr_resample);
+  cv::Mat resampledImage = ImgResample(image, w, h);
+  transpose(resampledImage, resampledImage); // JM: Si esto es necesario es porque está guardado mal en el YAML.
   
+  // Read the imResample resuls from Matlab.
   FileStorage fs1;
   FileStorage fs2;
   FileStorage fs3;
@@ -98,81 +93,111 @@ TEST_F(TestUtils, TestResampleColorImage)
 
   FileNode rows = fs1["res1"]["rows"];
   FileNode cols = fs1["res1"]["cols"];
-  
-  FileNode imReample1 = fs1["res1"]["data"];
-  FileNode imReample2 = fs2["res2"]["data"];
-  FileNode imReample3 = fs3["res3"]["data"];
-
-  cv::Mat bgr_dst0_chng;
-  bgr_dst[0].convertTo(bgr_dst0_chng, CV_32F);
-  //transpose(bgr_dst0_chng, bgr_dst0_chng);
-  float *data = bgr_dst0_chng.ptr<float>();
-
-  int difPixels = 0;
-  for(int j = 0; j < (int)cols; j++)
+  std::vector<cv::Mat> imResample_matlab(3);
+  for (int i=0; i<3; i++)
   {
-    for(int i = 0; i < (int)rows; i++)
-    {
-      if(abs(data[i + j] -  (float)imReample3[i + j])> 1)
-      {
-        difPixels = difPixels + 1;
-      }
-    }
+    imResample_matlab[i] = cv::Mat::zeros(rows, cols, CV_32FC1);
   }
+  FileNode imResample1 = fs1["res1"]["data"];
+  std::vector<float> p;
+  imResample1 >> p;
+  memcpy(imResample_matlab[0].data, p.data(), p.size()*sizeof(float));
+  FileNode imResample2 = fs2["res2"]["data"];
+  p.clear();
+  imResample2 >> p;
+  memcpy(imResample_matlab[1].data, p.data(), p.size()*sizeof(float));
+  FileNode imResample3 = fs3["res3"]["data"];
+  p.clear();
+  imResample3 >> p;
+  memcpy(imResample_matlab[2].data, p.data(), p.size()*sizeof(float));
 
-  ASSERT_TRUE(difPixels < 10);
+  // Split the channels of the resampled image.
+  std::vector<cv::Mat> resampledImage_channels(3);
+  split(resampledImage, resampledImage_channels);
 
-  cv::Mat bgr_dst1_chng;
-  bgr_dst[1].convertTo(bgr_dst1_chng, CV_32F);
-  float *data2 = bgr_dst1_chng.ptr<float>();
+  // Compare pixels from resampledImage and resampledImageMatlab
+  cv::Mat absDiff0 = cv::abs(resampledImage_channels[0] - imResample_matlab[0]);
+  cv::Mat absDiff1 = cv::abs(resampledImage_channels[1] - imResample_matlab[2]);
+  cv::Mat absDiff2 = cv::abs(resampledImage_channels[2] - imResample_matlab[2]);
+  cv::Mat res;
+  cv::bitwise_or((absDiff0 > 1), (absDiff1 > 1), res);
+  cv::bitwise_or((absDiff2 > 1), res, res);
+  int num_differences = cv::sum(res)[0];
 
-  int difPixels2 = 0;
-  for(int j = 0; j < (int)cols; j++)
-  {
-    for(int i = 0; i < (int)rows; i++)  
-    {
-      if(abs(data2[i + j] -  (float)imReample2[i + j])> 1)
-      {
-        difPixels2 = difPixels2 + 1;
-      }
-    }
-  }
+  std::cout << "num_differences=" << num_differences << std::endl;
+
+  ASSERT_TRUE(num_differences < 10);
+
+//  cv::Mat bgr_dst0_chng;
+//  bgr_dst[0].convertTo(bgr_dst0_chng, CV_32F);
+//  //transpose(bgr_dst0_chng, bgr_dst0_chng);
+//  float *data = bgr_dst0_chng.ptr<float>();
+
+//  int difPixels = 0;
+//  for(int j = 0; j < (int)cols; j++)
+//  {
+//    for(int i = 0; i < (int)rows; i++)
+//    {
+//      if(abs(data[i + j] -  (float)imReample3[i + j])> 1)
+//      {
+//        difPixels = difPixels + 1;
+//      }
+//    }
+//  }
+
+//  ASSERT_TRUE(difPixels < 10);
+
+//  cv::Mat bgr_dst1_chng;
+//  bgr_dst[1].convertTo(bgr_dst1_chng, CV_32F);
+//  float *data2 = bgr_dst1_chng.ptr<float>();
+
+//  int difPixels2 = 0;
+//  for(int j = 0; j < (int)cols; j++)
+//  {
+//    for(int i = 0; i < (int)rows; i++)
+//    {
+//      if(abs(data2[i + j] -  (float)imReample2[i + j])> 1)
+//      {
+//        difPixels2 = difPixels2 + 1;
+//      }
+//    }
+//  }
 
 
-  ASSERT_TRUE(difPixels2 < 1);
+//  ASSERT_TRUE(difPixels2 < 1);
 
-  cv::Mat bgr_dst2_chng;
-  bgr_dst[2].convertTo(bgr_dst2_chng, CV_32F);
-  float *data3 = bgr_dst2_chng.ptr<float>();
+//  cv::Mat bgr_dst2_chng;
+//  bgr_dst[2].convertTo(bgr_dst2_chng, CV_32F);
+//  float *data3 = bgr_dst2_chng.ptr<float>();
 
-  int difPixels3 = 0;
-  for(int j = 0; j < (int)cols; j++)
-  {
-    for(int i = 0; i < (int)rows; i++)
-    {
-      if(abs(data3[i + j] -  (float)imReample1[i + j])> 1)
-      {
-        difPixels3 = difPixels3 + 1;
-      }
-    }
-  }
+//  int difPixels3 = 0;
+//  for(int j = 0; j < (int)cols; j++)
+//  {
+//    for(int i = 0; i < (int)rows; i++)
+//    {
+//      if(abs(data3[i + j] -  (float)imReample1[i + j])> 1)
+//      {
+//        difPixels3 = difPixels3 + 1;
+//      }
+//    }
+//  }
 
-  ASSERT_TRUE(difPixels3 < 1);
+//  ASSERT_TRUE(difPixels3 < 1);
 
 
-  int difPixels4 = 0;
-  for(int j = 0; j < (int)cols; j++)
-  {
-    for(int i = 0; i < (int)rows; i++)
-    {
-      if(abs(data3[i + j] -  (float)imReample1[i + j])> 1 or abs(data2[i + j] -  (float)imReample2[i + j])> 1 or abs(data[i + j] -  (float)imReample3[i + j])> 1) 
-      {
-        difPixels4 = difPixels4 + 1;
-      }
-    }
-  }
+//  int difPixels4 = 0;
+//  for(int j = 0; j < (int)cols; j++)
+//  {
+//    for(int i = 0; i < (int)rows; i++)
+//    {
+//      if(abs(data3[i + j] -  (float)imReample1[i + j])> 1 or abs(data2[i + j] -  (float)imReample2[i + j])> 1 or abs(data[i + j] -  (float)imReample3[i + j])> 1)
+//      {
+//        difPixels4 = difPixels4 + 1;
+//      }
+//    }
+//  }
 
-  ASSERT_TRUE(difPixels4 < 1);
+//  ASSERT_TRUE(difPixels4 < 1);
 }
 
 TEST_F(TestUtils, TestResampleConv)
