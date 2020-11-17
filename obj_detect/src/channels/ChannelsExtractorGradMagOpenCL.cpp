376cf9f5465a529fd7ca3140d1b7fd65ac0d2a4d
jorgevelap <velajorge.93@gmail.com>
2020-11-17 18:51:44 +0100

    Actualizacion gradMagOpenCL

--- obj_detect/src/channels/ChannelsExtractorGradMagOpenCL.cpp
+++ obj_detect/src/channels/ChannelsExtractorGradMagOpenCL.cpp
@@ -22,8 +22,8 @@ void
 gradMagOpenCL
   (
   const cv::UMat I, // one channel
-  cv::Mat& M,
-  cv::Mat& O,
+  cv::UMat& M,
+  cv::UMat& O,
   bool full = true
   )
 {
@@ -38,15 +38,11 @@ gradMagOpenCL
   cv::filter2D(I, Gx, -1, kernel_horiz);
   cv::filter2D(I, Gy, -1, kernel_vert);*/
   //
+  cv::UMat Gx, Gy,  Gx2, Gy2;
   cv::Mat kernel_horiz = (cv::Mat_<float>(1,3) << 0.5, 0, -0.5);
-  cv::Mat kernel_vert  = (cv::Mat_<float>(3,1) << 0.5, 0, -0.5);
-  cv::UMat GxUMat;
-  cv::UMat GyUMat;
-  cv::filter2D(I, GxUMat, -1, kernel_horiz.getUMat( cv::ACCESS_READ ));
-  cv::filter2D(I, GyUMat, -1, kernel_vert.getUMat( cv::ACCESS_READ ));
-  //cv::Mat Gx = GxUMat.getMat( cv::ACCESS_READ );
-  //cv::Mat Gy = GyUMat.getMat( cv::ACCESS_READ );
-  //
+  //cv::Mat kernel_vert  = (cv::Mat_<float>(3,1) << 0.5, 0, -0.5);
+  cv::filter2D(I, Gx, -1, kernel_horiz);
+  cv::filter2D(I, Gy, -1, kernel_horiz.t());
 
   // Compute gradient mangitude (M)
   /*cv::Mat Gx2, Gy2;
@@ -55,14 +51,10 @@ gradMagOpenCL
   M = Gx2 + Gy2;
   cv::sqrt(M, M);
   M = cv::min(M, 1e10f);*/
-
-  cv::UMat Gx2_2, Gy2_2;
-  cv::multiply(GxUMat, GxUMat, Gx2_2);
-  cv::multiply(GyUMat, GyUMat, Gy2_2);
-  cv::UMat M_3;
-  add(Gx2_2, Gy2_2, M_3);
-  cv::UMat M_2 = M.getUMat( cv::ACCESS_READ ); // ?????
-  cv::sqrt(M_3, M_2);
+  cv::multiply(Gx, Gx, Gx2);
+  cv::multiply(Gy, Gy, Gy2);
+  cv::add(Gx2, Gy2, M);
+  cv::sqrt(M, M);
   //M = M_2.getMat( cv::ACCESS_READ );
   //M = cv::min(M, 1e10f);
 
@@ -72,9 +64,9 @@ gradMagOpenCL
   cv::phase(Gx, Gy, O, false); // Compute angle in radians in [0, 2pi)*/
 
   //cv::UMat M_2 = M.getUMat( cv::ACCESS_READ );
-  cv::multiply(GxUMat, M_2, GxUMat);
-  cv::multiply(GyUMat, M_2, GyUMat); 
-  cv::phase(GxUMat, GyUMat, O, false);
+  cv::multiply(Gx, M, Gx);
+  cv::multiply(Gy, M, Gy); 
+  cv::phase(Gx, Gy, O, false);
 
   //cv::Mat Gy = GyUMat.getMat( cv::ACCESS_READ );
   /*if (!full)
@@ -90,16 +82,17 @@ gradMagOpenCL
   if (!full)
   {
     // Remove M_PI to orientations with Gy < 0 to get orientation in [0, pi)
-    cv::multiply(GyUMat, -1, GyUMat);
-    cv::threshold( GyUMat, GyUMat, 0, 255, 0 );
-    cv::divide( GyUMat, 255, GyUMat);
+    cv::multiply(Gy, -1, Gy);
+    cv::threshold( Gy, Gy, 0, 255, 0 );
+    cv::divide( Gy, 255, Gy);
 
-    cv::UMat isGyNegFloat;
-    GyUMat.convertTo(isGyNegFloat, CV_32F);
-    cv::UMat toSustract;
-    cv::multiply(isGyNegFloat, M_PI, toSustract);
-    cv::Mat Gy = toSustract.getMat( cv::ACCESS_READ );
-    O -= Gy;
+    //cv::UMat isGyNegFloat;
+    //GyUMat.convertTo(isGyNegFloat, CV_32F);
+    //cv::UMat toSustract;
+    cv::multiply(Gy, M_PI, Gy);
+    cv::subtract(O, Gy, O);
+    //cv::Mat Gy = toSustract.getMat( cv::ACCESS_READ );
+    //O -= Gy;
   }
 }
 
@@ -119,17 +112,30 @@ ChannelsExtractorGradMagOpenCL::extractFeatures
 
   cv::Size orig_sz = img.size();
   std::vector<cv::Mat> channelsGradMag(2);
-  channelsGradMag[0] = cv::Mat::zeros(orig_sz.height, orig_sz.width, CV_32FC1); // M
-  channelsGradMag[1] = cv::Mat::zeros(orig_sz.height, orig_sz.width, CV_32FC1); // O
+
+  cv::UMat channelsGradMag_0 = cv::UMat::zeros(orig_sz.height, orig_sz.width, CV_32FC1);
+  cv::UMat channelsGradMag_1 = cv::UMat::zeros(orig_sz.height, orig_sz.width, CV_32FC1);
+
+
+
+  //channelsGradMag[0] = cv::Mat::zeros(orig_sz.height, orig_sz.width, CV_32FC1); // M
+  //channelsGradMag[1] = cv::Mat::zeros(orig_sz.height, orig_sz.width, CV_32FC1); // O
   if (nChannels == 1)
   {
     cv::UMat img_float;
     IM.convertTo(img_float, CV_32FC1); // important to have continuous memory in img_aux.ptr<float>
     auto start = std::chrono::system_clock::now(); 
-    gradMagOpenCL(img_float, channelsGradMag[0], channelsGradMag[1], false);  
+    gradMagOpenCL(img_float, channelsGradMag_0, channelsGradMag_1, false);  
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<float,std::milli> duration = end - start;
     std::cout << duration.count() << " time in OpenCL" << std::endl;
+    cv::Mat chn0 = channelsGradMag_0.getMat(cv::ACCESS_READ);
+    cv::Mat chn1 = channelsGradMag_1.getMat(cv::ACCESS_READ);
+
+    chn0.copyTo(channelsGradMag[0]);
+    chn1.copyTo(channelsGradMag[1]);
+    //channelsGradMag[0] = channelsGradMag_0.getMat(cv::ACCESS_READ);
+    //channelsGradMag[1] = channelsGradMag_1.getMat(cv::ACCESS_READ);
   }
   else if (nChannels == 3)
   {/*
